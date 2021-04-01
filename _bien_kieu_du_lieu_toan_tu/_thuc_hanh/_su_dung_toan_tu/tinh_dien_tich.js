c8dd869827391743142e8aa910b66c8656b420f3
nguyennguyen410 <nguyen.nhn89@gmail.com>
2021-04-01 15:57:25 +0700

    thuc hanh bien, kieu du lieu va toan tu

---
+++ _bien_kieu_du_lieu_toan_tu/_thuc_hanh/_su_dung_toan_tu/tinh_dien_tich.js
@@ -0,0 +1,6 @@
+let inputWidth=prompt("Enter the Width");
+let inputHeight=prompt("Enter the Height");
+let width=parseInt(inputWidth);
+let height=parseInt(inputHeight);
+let area=width*height;
+document.write("The area is: "+area)
